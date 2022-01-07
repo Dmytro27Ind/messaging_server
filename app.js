c86349af59bfc5ed3dcc11217654b77148f08f6d
Dmytro <dmytro27kagirov@gmail.com>
2022-01-07 02:23:18 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -4,18 +4,24 @@ const Schema = mongoose.Schema;
 const app = express();
 const jsonParser = express.json();
 
-const userScheme = new Schema({id: String, name: String, email: String, photo: String,
-    chats: {
-        type: [{
-            with_id: String,
-            messages: [String]
-        }],
-        required: false
-    }
+const userScheme = new Schema({
+    id: String,
+    name: String,
+    email: String,
+    photo: String,
+}, {versionKey: false});
 
+const messagesScheme = new Schema({
+    id: String,
+    with_id: String,
+    messages: [{
+        id: String,
+        message: String
+    }]
 }, {versionKey: false});
 
 const User = mongoose.model("user", userScheme);
+const Messages = mongoose.model("messages", messagesScheme);
 
 const PORT = process.env.PORT || 3000
 
@@ -39,11 +45,29 @@ app.get("/api/users/:id", function(request, response){
     });
 });
 
+app.get("/api/messages/:id/:with_id", function(request, response){
+    const id = request.params.id;
+    const with_id = request.params.with_id
+    Messages.findOne({id: id, with_id: with_id}, function(error, messages){
+        if(error)
+            return console.log(error);
+        response.send(messages);
+    });
+});
+
 app.get("/api/users", function(request, response){
     User.find({}, function(error, users){
         if(error)
             return console.log(error);
         response.send(users)
+    });
+});
+
+app.get("/api/messages", function(request, response){
+    Messages.find({}, function(error, messages){
+        if(error)
+            return console.log(error);
+        response.send(messages)
     });
 });
 
@@ -62,6 +86,44 @@ app.post("/api/users", jsonParser, function (request, response) {
         if(error)
             return console.log(error);
         response.send(user);
+    });
+});
+
+app.post("/api/messages", jsonParser, function (request, response) {
+    if(!request.body)
+        return response.sendStatus(400);
+
+    const userId = request.body.id;
+    const withId = request.body.with_id;
+    const userMessage = request.body.messages;
+
+    Messages.findOne({id: userId}, function(error, messages){
+        if(error)
+            return console.log(error);
+        if(messages == null) {
+            const message = new Messages({id: userId, with_id: withId, messages: userMessage});
+            message.save(function (error) {
+                if (error)
+                    return console.log(error);
+                response.send(message);
+            });
+        } else{
+            let mes = []
+            for(let m of messages.messages)
+                mes.push({id: m.id, message: m.message})
+            mes.push(userMessage[0])
+            const updateMessages = new Messages({id: userId, with_id: withId, messages: mes});
+
+            Messages.findOneAndDelete({id: userId, with_id: withId}, function(error, messages){
+                if(error)
+                    return console.log(error);
+            });
+            updateMessages.save(function (error) {
+                if (error)
+                    return console.log(error);
+                response.send(updateMessages);
+            });
+        }
     });
 });
 
